c8d6d31f9560cc9a34e52a9788b7a2e48b3468f0
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2025-01-25 19:43:44 -0300

    se aplicaron los estilos a la columna del texto del componente hero y se agrego el boton de ver mas

--- src/components/homeComponents/about/About.jsx
+++ src/components/homeComponents/about/About.jsx
@@ -10,17 +10,20 @@ function About() {
           <img src={exampleImage} alt="Descripción de la imagen" />
         </div>
         <div className="column text-column">
-          <h2>Sobre Nosotros</h2>
-          <p>
-          Andinos es un restaurante que celebra la tradición del norte 
-          argentino mientras ofrece los clásicos favoritos que todos disfrutan. 
-          Nuestra propuesta combina lo mejor de ambas experiencias: 
-          platos tradicionales como empanadas salteñas, locros, 
-          humitas y tamales se mezclan con opciones modernas como 
-          hamburguesas jugosas, papas fritas crocantes, pizzas irresistibles y más. 
-          Cada bocado es una invitación a explorar sabores auténticos, 
-          ideales para compartir momentos inolvidables en un ambiente cálido y acogedor.
-          </p>
+            <h2>- Sobre Nosotros -</h2>
+            <p>
+                Andinos es un restaurante que celebra la tradición del norte 
+                argentino mientras ofrece los clásicos favoritos que todos disfrutan. 
+                Nuestra propuesta combina lo mejor de ambas experiencias: 
+                platos tradicionales como empanadas salteñas, locros, 
+                humitas y tamales se mezclan con opciones modernas como 
+                hamburguesas jugosas, papas fritas crocantes, pizzas irresistibles y más. 
+                Cada bocado es una invitación a explorar sabores auténticos, 
+                ideales para compartir momentos inolvidables en un ambiente cálido y acogedor.
+            </p>
+            <button className="btnAbout">
+                <span>Ver Más</span>
+            </button>
         </div>
       </div>
     </div>
