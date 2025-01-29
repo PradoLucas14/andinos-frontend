58158b6ed29fc196c81caa403a0b327c612f9566
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2025-01-28 22:17:29 -0300

    se modificaron los estilos de la hoja de estilos del comonent products

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./Home.css"
 import Hero from '../../components/homeComponents/hero/Hero'
 import About from '../../components/homeComponents/about/About'
+import Products from '../../components/homeComponents/products/Products'
 
 function Home() {
   return (
     <div className='Home'>
       <Hero/>
       <About/>
+      <Products/>
     </div>
   )
 }
