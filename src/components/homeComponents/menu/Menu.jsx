d32fb12e40329869e1598962f1e053d8a1d8d0db
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2025-02-02 22:23:42 -0300

    se creo el Slider con las cartas para el componente Menu

--- src/components/homeComponents/menu/Menu.jsx
+++ src/components/homeComponents/menu/Menu.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
-import "./Menu.css"
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./Menu.css";
+
+// Importa las imágenes
+import img1 from "../../../assets/img/imgMenu1.jpg";
+import img2 from "../../../assets/img/imgMenu2.jpg";
+import img3 from "../../../assets/img/imgMenu3.jpg";
+import img4 from "../../../assets/img/imgMenu4.jpg";
+import img5 from "../../../assets/img/imgMenu5.jpg";
+import img6 from "../../../assets/img/Imgmenu6.avif";
 
 function Menu() {
+  const menuItems = [
+    { src: img1, title: "Empanadas", price: "$500" },
+    { src: img2, title: "Hamburgueza", price: "$3500" },
+    { src: img3, title: "Pizza", price: "$6100" },
+    { src: img4, title: "Tacos de Carne", price: "$800" },
+    { src: img5, title: "Ñoquis", price: "$3800" },
+    { src: img6, title: "Milanesa", price: "$4400" }
+  ];
+
   return (
-    <div>
-      hola soy un menu
+    <div className="menu-container">
+      <h2 className="menu-title">- Menú del Día -</h2>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={20}
+        slidesPerView={1}
+        loop={true}
+        autoplay={{ delay: 2500, disableOnInteraction: false }}
+        navigation
+        pagination={{ clickable: true }}
+        breakpoints={{
+          640: { slidesPerView: 1 },
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 }
+        }}
+        className="menu-swiper"
+      >
+        {menuItems.map((item, index) => (
+          <SwiperSlide key={index}>
+            <div className="menu-card">
+              <img src={item.src} alt={item.title} className="menu-image" />
+              <div className="menu-info">
+                <h3 className="menu-item-title">{item.title}</h3>
+                <p className="menu-item-price">{item.price}</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
+
